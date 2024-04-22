ab73fc26afe6187420c47aa1242d7fcc4f5914b6
glazarev <gleb.lazarev20@yandex.ru>
2024-04-22 14:52:52 +0300

    Печатается credentials

--- app.py
+++ app.py
@@ -29,15 +29,18 @@ async def root(request: Request, path: str):
         payload = await request.json()
     except JSONDecodeError:
         payload = None
+
+    with_credentials = payload.get("withCredentials") if payload else None
+
     if request.method == "GET":
-        return JSONResponse({"message": f"GET-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}"}, status_code=200)
+        return JSONResponse({"message": f"GET-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}, withCredentials={with_credentials}"}, status_code=200)
     elif request.method == "POST":
-        return JSONResponse({"message": f"POST-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}"}, status_code=200)
+        return JSONResponse({"message": f"POST-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}, withCredentials={with_credentials}"}, status_code=200)
     elif request.method == "DELETE":
-        return JSONResponse({"message": f"DELETE-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}"}, status_code=200)
+        return JSONResponse({"message": f"DELETE-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}, withCredentials={with_credentials}"}, status_code=200)
     elif request.method == "PATCH":
-        return JSONResponse({"message": f"PATCH-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}"}, status_code=200)
+        return JSONResponse({"message": f"PATCH-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}, withCredentials={with_credentials}"}, status_code=200)
     elif request.method == "PUT":
-        return JSONResponse({"message": f"PUT-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}"}, status_code=200)
+        return JSONResponse({"message": f"PUT-запрос, путь=/{path}, params={dict(request.query_params)}, headers={headers}, data={payload}, withCredentials={with_credentials}"}, status_code=200)
 
 app.include_router(router)
